fb6163fd5249f2648b771e37dec16fe5d5e3aa62
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-01-11 18:33:07 -0600

    Add api call

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-export const GifGrid = ({category}) => {
-    return (
-        <div>
-            <h3>{category}</h3>
-        </div>
-    )
-}
+export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=pokemon&limit=10&api_key=";
+    const response = await fetch(url);
+    const { data } = await response.json();
+
+    const gifts = data.map(img =>{
+        return {
+            id:img.id,
+            title:img.title,
+            url: img.images?.downsized_medium?.url
+        }
+    })
+    console.log(gifts);
+  };
+  useEffect(() => {
+    getGifs();
+  }, []);
+  return (
+    <div>
+      <h3>{category}</h3>
+    </div>
+  );
+};
